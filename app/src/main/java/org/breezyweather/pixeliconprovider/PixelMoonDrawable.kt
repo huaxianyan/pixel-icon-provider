15d4f67bb6f7337181e7c20796f125a3c26c4bb5
Neko Nanaina <hua199057@gmail.com>
2026-07-05 11:10:16 +0800

    修正一处RGB数值

--- app/src/main/java/org/breezyweather/pixeliconprovider/PixelMoonDrawable.kt
+++ app/src/main/java/org/breezyweather/pixeliconprovider/PixelMoonDrawable.kt
@@ -35,7 +35,7 @@ class PixelMoonDrawable : Drawable() {
     }
     private val mClearXfermode: Xfermode
 
-    private val mCoreColor: Int = Color.rgb(169, 217, 255)
+    private val mCoreColor: Int = Color.rgb(196, 217, 255)
     private var mAlpha: Float = 1f
     private var mBounds: Rect
     private var mCoreRadius = 0f
